ce073492456026530226b8f7444e80d52afe0b6e
GonzaloPereda <63264439+GonzaloPereda@users.noreply.github.com>
2021-09-12 02:18:07 -0300

    Levante react y arme un menu de prueba

---
+++ front/gimnasio/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PageHome from "./page-home.js";
+
+function App() {
+    return (
+        <div class="nav-bg">
+            <nav class="navegacion-principal contenedor">
+                <a href="#">Inicio</a>
+                <a href="#">Informacion</a>
+                <a href="#">Iniciar sesion</a>
+            </nav>
+        </div>
+    );
+}
+
+export default App;
---
+++ front/gimnasio/src/page-home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./page-home.css";
+
+class PageHome extends React.Component {
+    render() {
+        return <span className="nav-bar">Menu</span>;
+    }
+}
+
+export default PageHome;
